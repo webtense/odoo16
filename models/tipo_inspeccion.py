7ac392ac1e66222ec315316a2d9c9d6ef472c2d0
root <root@DebOdoo16.boitaull.com>
2024-12-04 20:13:59 +0000

    Actualización de versión a 16.0.1.7

--- models/tipo_inspeccion.py
+++ models/tipo_inspeccion.py
@@ -1,6 +1,6 @@
 # Fecha: 2024-12-04
 # Nombre del archivo: tipo_inspeccion.py
-# Versión del archivo: V1
+# Versión del archivo: V2
 
 from odoo import models, fields
 
@@ -8,5 +8,5 @@ class TipoInspeccion(models.Model):
     _name = 'btr.tipo.inspeccion'
     _description = 'Tipo de Inspección'
 
-    name = fields.Char(string="Nombre", required=True)
-    descripcion = fields.Text(string="Descripción")
+    name = fields.Char(string="Nombre del Tipo", required=True)
+    description = fields.Text(string="Descripción", help="Detalles adicionales sobre el tipo de inspección.")
