4a5d59be8996dfc732cbd1400d7a28fd1d0616a5
root <root@DebOdoo16.boitaull.com>
2024-12-04 16:42:45 +0000

    Actualización de versión a 16.0.1.2

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Gestión de Inspecciones Obligatorias',
-    'version': '16.0.1.1',
+    'version': '16.0.1.2',
     'summary': 'Gestión centralizada de inspecciones obligatorias en instalaciones hoteleras.',
     'description': """
         Este módulo permite gestionar inspecciones obligatorias, incluyendo tipos, responsables y resultados.
